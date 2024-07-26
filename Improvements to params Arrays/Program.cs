44c35194e233c44924538aa32558a68fa66b32d2
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-26 09:29:45 +0200

    Add project files.

---
+++ Improvements to params Arrays/Program.cs
@@ -0,0 +1,9 @@
+﻿PrintNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+static void PrintNumbers(params int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.WriteLine(number);
+    }
+}
